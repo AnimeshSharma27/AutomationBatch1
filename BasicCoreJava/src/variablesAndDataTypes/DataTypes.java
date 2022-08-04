68aacbff01d2e707ab03e9e9879868802c6742f9
Animesh Sharma <Animesh.Sharma@neudesic.com>
2022-08-04 17:15:20 +0530

    First time commiting on git

---
+++ BasicCoreJava/src/variablesAndDataTypes/DataTypes.java
@@ -0,0 +1,28 @@
+package variablesAndDataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           
+		int age=33;
+		String name="Animesh";
+		char gender='M';
+		boolean marritialStatus=true;
+		double height=5.8;
+		
+		
+		System.out.println(age);
+		
+		
+		// what is string , it is a datatype or class. A: it is a class
+		
+		//primitive and non primitives
+		
+		// primitives : byte,short,int,long,char,double,float,boolean
+		
+		// classes,interfaces,arrays,collections
+		
+	}
+
+}
